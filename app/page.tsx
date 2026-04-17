c6f0b865101994bddacb555c99581944cb0dc8c3
Nimdal <0xnimdal@gmail.com>
2026-04-18 04:03:24 +0900

    Pin uptime base timestamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { TerminalShell } from "@/components/terminal-shell";
 import IntroContent from "@/content/intro.mdx";
 
-const SERVER_BOOTED_AT = Date.now();
+const SERVER_BOOTED_AT = new Date("2026-04-14T01:00:00+09:00").getTime();
 
 export default function Home() {
   return (
